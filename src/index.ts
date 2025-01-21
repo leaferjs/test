92da569fc30afe2bbbb267cd31f970ee23412cae
leaferjs <leaferjs@163.com>
2025-01-21 08:35:18 +0800

    v1.3.3

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import * as skia from 'skia-canvas'
+//import * as skia from 'skia-canvas'
+import skia from '@napi-rs/canvas'
 import { useCanvas } from '@leafer-ui/node'
 import '@leafer-in/viewport'
 import '@leafer-in/find'
 import '@leafer-in/export'
 
-useCanvas('skia', skia)
+//useCanvas('skia', skia)
+useCanvas('napi', skia) // must
 
 export * from '@leafer-ui/node'
-
